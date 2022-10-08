1a2195440d787ca60f6f15f974efea65bb86527a
Caleb Lamoreaux <caleblamro@gmail.com>
2022-10-07 21:47:19 -0700

    Testing different users

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ async function main(){
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     console.info("------STARTING ANALYSIS------");
-    const userURL = 'https://www.tiktok.com/@charlidamelio';
+    const userURL = 'https://www.tiktok.com/@ninja';
     console.info("------GETTING USER DATA------");
     const userData = await createUserData(page, userURL);
     console.log(userData.name);
@@ -55,7 +55,7 @@ async function main(){
     console.log("USER VIDEOS: ");
     //only loop through constant number of videos to avoid errors
     console.info("------GETTING SAMPLE VIDEO DATA------");
-    for(i = 0; i < userData.video_links.length; i++){
+    for(i = 0; i < 3; i++){
         console.log("\t\t" + (i+1) + " : " + userData.video_links[i]);
         const videoData = await getVideoInfo(page, userData.video_links[i]);
         userData.video_data[i] = videoData;
